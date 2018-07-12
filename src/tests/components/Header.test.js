c73085bcfc9750a525fca12ac4f3ccf8c9da416b
lyt2015 <lyt2015@users.nonreply.github.com>
2018-07-12 23:07:02 +0800

    Add tests for simple react components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-// import toJSON from 'enzyme-to-json'
-// import ReactShallowRenderer from 'react-test-renderer/shallow'
 
 import Header from '../../components/Header'
 
@@ -9,15 +7,4 @@ test('should render Header correctly', () => {
   const wrapper = shallow(<Header />)
 
   expect(wrapper).toMatchSnapshot()
-  // expect(toJSON(wrapper)).toMatchSnapshot()
-
-  // expect(wrapper.find('h1').length).toBe(1)
-  // expect(wrapper.find('h1').text()).toBe('Expensify')
-
-  /*
-  const renderer = new ReactShallowRenderer()
-  renderer.render(<Header />)
-
-  expect(renderer.getRenderOutput()).toMatchSnapshot()
- */
 })
