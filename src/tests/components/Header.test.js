2692eda264c7ef57070ba16f7e3b2995f4bdb571
lyt2015 <lyt2015@users.nonreply.github.com>
2018-07-12 20:35:36 +0800

    Add enzyme for testing react components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
+// import toJSON from 'enzyme-to-json'
+// import ReactShallowRenderer from 'react-test-renderer/shallow'
 
 import Header from '../../components/Header'
 
 test('should render Header correctly', () => {
+  const wrapper = shallow(<Header />)
+
+  expect(wrapper).toMatchSnapshot()
+  // expect(toJSON(wrapper)).toMatchSnapshot()
+
+  // expect(wrapper.find('h1').length).toBe(1)
+  // expect(wrapper.find('h1').text()).toBe('Expensify')
+
+  /*
   const renderer = new ReactShallowRenderer()
   renderer.render(<Header />)
 
   expect(renderer.getRenderOutput()).toMatchSnapshot()
+ */
 })
